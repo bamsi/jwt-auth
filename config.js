0c74bf6d78da69a28c167d2e7e714b1e7b43c961
Haji Bamsi <hibamsi@gmail.com>
2025-06-14 01:33:19 +0300

    fix error

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  SUPERSET_SECRET_KEY: 'YOUR_SUPERSET_GUEST_TOKEN_SECRET_KEY_GOES_HERE', // IMPORTANT: Replace with your actual secret key
-  JWT_EXPIRY: '1h', // Token expiry (e.g., 1h, 5m, 24h)
-  SUPERSET_URL: 'http://localhost:8088', // Your Superset instance URL
+  SUPERSET_SECRET_KEY:
+    "akuX6dAE/GwyiLHviFtz5JH28+c2zU/P0m1TvNLYeOIfC9LOO6/U5+tx", // IMPORTANT: Replace with your actual secret key
+  JWT_EXPIRY: "1h", // Token expiry (e.g., 1h, 5m, 24h)
+  SUPERSET_URL: "http://102.223.7.208:8090", // Your Superset instance URL
   // Define default user and resource structures for the guest token if needed,
   // or these can be fully dynamic based on request body.
   // Example structure for a guest user:
